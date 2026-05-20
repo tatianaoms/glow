0161c5293ad6fc4ff77a41c74149514d2539b44c
Tatiana Ordoñez <tatiana.2004.morales@gmail.com>
2026-05-20 12:08:12 -0500

    Fix: Limpieza de la tabla menus

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -9,18 +9,15 @@ class MenuSeeder extends Seeder
 {
     public function run(): void
     {
-        \Illuminate\Support\Facades\Schema::disableForeignKeyConstraints();
 
-
-        \App\Models\Menu::truncate();
-
-        \Illuminate\Support\Facades\Schema::enableForeignKeyConstraints();
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        \Illuminate\Support\Facades\DB::table('menus')->truncate();
+        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
 
         $inicio = \App\Models\Menu::create(['id' => 1, 'nombre' => 'INICIO', 'url' => '/', 'parent_id' => null, 'orden' => 1]);
         $categorias = \App\Models\Menu::create(['id' => 2, 'nombre' => 'CATEGORÍAS', 'url' => '#', 'parent_id' => null, 'orden' => 3]);
         $recientes = \App\Models\Menu::create(['id' => 7, 'nombre' => 'RECIENTES', 'url' => '/tips', 'parent_id' => null, 'orden' => 2]);
-
 
         \App\Models\Menu::create(['id' => 4, 'nombre' => 'Maquillaje', 'url' => '/categoria/maquillaje', 'parent_id' => 2, 'orden' => 1]);
         \App\Models\Menu::create(['id' => 5, 'nombre' => 'Facial', 'url' => '/categoria/facial', 'parent_id' => 2, 'orden' => 2]);
