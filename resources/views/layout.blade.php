6c8c99a26b421fa9be897e637019e46d4d7d1d40
Tatiana Ordoñez <tatiana.2004.morales@gmail.com>
2026-05-20 23:54:33 -0500

    shine

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
 <body class="{{ request()->cookie('tema_preferido') == 'dark' ? 'dark-mode' : '' }}">
 
     <div class="top-bar">
-        ¡NUEVOS TIPS DE BELLEZA CADA SEMANA! EXPLORA TU PROPIO GLOW - BY TATI
+        ¡NUEVOS TIPS DE BELLEZA CADA SEMANA! EXPLORA TU PROPIO GLOW - SHINE
     </div>
 
     <header class="main-header">
