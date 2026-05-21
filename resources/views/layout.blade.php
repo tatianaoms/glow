6fa1d6c67239d06113d60b31005c64833120c4dd
tatianaoms <tatianaoms@gmail.com>
2026-05-21 02:30:47 +0000

    test webhook automático

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
 <body class="{{ request()->cookie('tema_preferido') == 'dark' ? 'dark-mode' : '' }}">
 
     <div class="top-bar">
-        ¡NUEVOS TIPS DE BELLEZA CADA SEMANA! EXPLORA TU PROPIO GLOW
+        ¡NUEVOS TIPS DE BELLEZA CADA SEMANA! EXPLORA TU PROPIO GLOW - BRILLA CON LUZ PROPIA
     </div>
 
     <header class="main-header">
